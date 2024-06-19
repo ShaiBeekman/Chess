7aef2a65d0a9b8153760b924f517a3992691a414
Shai Beekman <shaibeekman@gmail.com>
2024-06-18 22:52:10 -0400

    adding Chess.java

---
+++ Chess.java
@@ -0,0 +1,13 @@
+public class Chess {
+    static int[][] chessBoard = chessBoard[8][8]
+    static int[] chessBoard = int[64]
+    public static void main(String[] args) {
+        chessBoard();
+    }
+    chessBoard{
+        for (int i <;)
+            int j < ;){
+
+        }
+    }
+}
