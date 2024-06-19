2e7d233ffd756f180b140629d448ab3230cc78cc
Shai Beekman <shaibeekman@gmail.com>
2024-06-18 22:29:17 -0400

    add Knight.java

---
+++ Pawn.java
@@ -0,0 +1,7 @@
+package Chessz;
+public class Pawn {
+    Pawn[] pawns = new Pawn[]; //set of all kings created are stored here, will be useful for superchess once chess is solved
+    public Pawn () {
+        Pawn pawn = new Pawn;
+    }
+}
