cf8a40504fc9a049487330db2cdbe460b8974c51
Shai Beekman <shaibeekman@gmail.com>
2024-06-18 22:29:27 -0400

    add Knight.java

---
+++ Queen.java
@@ -0,0 +1,7 @@
+package Chessz;//import java.util.Random;
+public class Queen {
+    Queen[] queens = new Queen[]; //set of all kings created are stored here, will be useful for superchess once chess is solved
+    public Queen () {
+        Queen Queen = new Queen;
+    }
+}
