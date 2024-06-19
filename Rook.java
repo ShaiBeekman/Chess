f6bcd9f770334c2228a191b53588f146e602406a
Shai Beekman <shaibeekman@gmail.com>
2024-06-18 22:30:26 -0400

    add Rook.java

---
+++ Rook.java
@@ -0,0 +1,7 @@
+package Chess;
+public class Rook {
+    Rook[] rook = new Rook[]; //set of all kings created are stored here, will be useful for superchess once chess is solved
+    public Rook () {
+        Rook rook = new Rook;
+    }
+}
