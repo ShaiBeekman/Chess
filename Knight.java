e5215808d7f0b86232e5f2a0ad8da79df94cc1a1
Shai Beekman <shaibeekman@gmail.com>
2024-06-18 22:28:55 -0400

    add Knight.java

---
+++ Knight.java
@@ -0,0 +1,6 @@
+public class Knight {
+    Knight[] knights = new Knight[]; //set of all kings created are stored here, will be useful for superchess once chess is solved
+    public Knight () {
+        Knight knight = new Knight;
+    }
+}
